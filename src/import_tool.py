463c7b54a07e64851ce5f43d0da9ac51bb9773b8
yuvtorres <yuvtorres@gmail.com>
2020-04-10 19:28:55 +0200

    backup

---
+++ src/import_tool.py
@@ -0,0 +1,23 @@
+# funciones para importar las datos
+
+import pandas as pd
+import numpy as np
+
+def import_data_poetry(path):
+    df=pd.read_csv(path)
+    print(f"Data imported from -->  ""{path}""")
+    print(f"{len(df)} lines was imported")
+    columnas_str='\n'.join((list(df.columns)))
+    print(f'The columns are:\n{columnas_str}')
+    df=df.drop(['Unnamed: 0'],axis=1)
+    print(f"The column 'Unnamed: 0' was dropped.")
+    df['count_l']=df['Poem'].apply(lambda x:len(x.split('\n')))
+    print('the column count_l (numbers of line by poem) was creatted, description:')
+    print(df['count_l'].describe())
+    df['count_w']=df['Poem'].apply(lambda x:len(x.replace('\n',' ').split(' ')))
+    print('the column count_w (numbers of words by poem) was creatted, description:')
+    print(df['count_w'].describe())
+
+
+
+
